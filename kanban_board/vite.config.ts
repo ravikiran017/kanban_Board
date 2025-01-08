b09ec9cec9d9c65c13b2921359625f13515deee4
ravikiran017 <ravikirang2129@gmail.com>
2025-01-08 13:45:10 +0530

    hash route is adde to the mian tsx file

--- kanban_board/vite.config.ts
+++ kanban_board/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Ensure the output is in the 'dist' folder
-  },
+  base: '/kanban_Board/', // Correctly set to the repository name
 });
+
